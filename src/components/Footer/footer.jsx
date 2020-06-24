88ea7045b71ffa0b308ee73f574119de3aac1741
nagoor kani sheikmaideen <nagoorkanisheikmaideen@nagoors-MacBook-Pro.local>
2020-06-23 23:29:18 -0500

    refactored to react standards

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,73 +1,80 @@
-import React, { Component } from 'react';
-import { Card, Accordion,Image } from 'react-bootstrap';
-import './footer.css'
+import React, { Component } from "react";
+import { Card, Accordion, Image } from "react-bootstrap";
+import "./footer.css";
 class Contact extends Component {
   render() {
-      const links=[{
-          href:"https://syedengg.ac.in/index.php",
-          name:"Home"
+    const links = [
+      {
+        href: "https://syedengg.ac.in/index.php",
+        name: "Home",
       },
       {
-          href:"https://syedengg.ac.in/aboutus-saec.php",
-          name:"About Us"
+        href: "https://syedengg.ac.in/aboutus-saec.php",
+        name: "About Us",
       },
       {
-          href:"https://syedengg.ac.in/departments.php",
-          name:"Departments"
+        href: "https://syedengg.ac.in/departments.php",
+        name: "Departments",
       },
       {
-          href:"https://syedengg.ac.in/menshostel.php",
-          name:"Students"
+        href: "https://syedengg.ac.in/menshostel.php",
+        name: "Students",
       },
       {
-        href:"https://syedengg.ac.in/Research-Director.php",
-        name:"Research"
+        href: "https://syedengg.ac.in/Research-Director.php",
+        name: "Research",
       },
       {
-        href:"http://syedengg.indiacareerportal.com",
-        name:"Placement"
+        href: "http://syedengg.indiacareerportal.com",
+        name: "Placement",
       },
       {
-        href:"https://syedengg.ac.in/library_details.php",
-        name:"Library"
+        href: "https://syedengg.ac.in/library_details.php",
+        name: "Library",
       },
       {
-        href:"https://syedengg.ac.in/gallery.php",
-        name:"Gallery"
+        href: "https://syedengg.ac.in/gallery.php",
+        name: "Gallery",
       },
       {
-        href:"https://syedengg.ac.in/grps.php",
-        name:"Alumni"
+        href: "https://syedengg.ac.in/grps.php",
+        name: "Alumni",
       },
       {
-        href:"https://mail.google.com/a/syedengg.ac.in",
-        name:"WebMail"
+        href: "https://mail.google.com/a/syedengg.ac.in",
+        name: "WebMail",
       },
       {
-        href:"https://syedengg.ac.in/contact-us.php",
-        name:"Contact Us"
-      } 
-    ]
+        href: "https://syedengg.ac.in/contact-us.php",
+        name: "Contact Us",
+      },
+    ];
     return (
-
-        <Accordion>
+      <Accordion>
         <Card className="footer">
-        <p>
-        {links.map((l) => {
-            
-            return(
+          <p>
+            {links.map((l) => {
+              return (
                 <>
-                  <a href={l.href}> {l.name} </a><span class="fpad">|  </span>
+                  <a href={l.href}> {l.name} </a>
+                  <span class="fpad">| </span>
                 </>
-            );
-                    })}
-        </p>
-        <p>Copyright © 2011 - 2020. All Rights Reserved. Syed Ammal Engineering College.
-          <span className="id">Designed & Maintained by : </span></p>
-        <p><a href="http://www.syedengg.ac.in/">www.syedengg.ac.in</a>
-        <span className="logo"><Image src="asserts/images.png" alt="image" /></span></p>
-    </Card>
-    </Accordion>
+              );
+            })}
+          </p>
+          <p>
+            Copyright © 2011 - 2020. All Rights Reserved. Syed Ammal Engineering
+            College.
+            <span className="id">Designed & Maintained by : </span>
+          </p>
+          <p>
+            <a href="http://www.syedengg.ac.in/">www.syedengg.ac.in</a>
+            <span className="logo">
+              <Image src="asserts/images.png" alt="image" />
+            </span>
+          </p>
+        </Card>
+      </Accordion>
     );
   }
 }
